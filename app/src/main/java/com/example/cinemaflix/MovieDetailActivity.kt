f0f203e3497ec78809acfc512168ece9d4a33cbe
Moisis <moisis.george@yahoo.com>
2022-09-14 01:50:40 +0200

    Movie Detail  Done

--- app/src/main/java/com/example/cinemaflix/MovieDetailActivity.kt
+++ app/src/main/java/com/example/cinemaflix/MovieDetailActivity.kt
@@ -1,33 +1,56 @@
 package com.example.cinemaflix
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import com.bumptech.glide.Glide
-import com.example.cinemaflix.api.ApiInterface
-import com.example.cinemaflix.api.MovieApiService
-import com.example.cinemaflix.models.MovieDetail
-import com.example.cinemaflix.models.MovieDetailResponse
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_movie_detail.*
-import kotlinx.android.synthetic.main.movie_item.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
+
+const val MOVIE_BACKDROP = "extra_movie_backdrop"
+const val MOVIE_POSTER = "extra_movie_poster"
+const val MOVIE_TITLE = "extra_movie_title"
+const val MOVIE_RATING = "extra_movie_rating"
+const val MOVIE_RELEASE_DATE = "extra_movie_release_date"
+const val MOVIE_OVERVIEW = "extra_movie_overview"
 class MovieDetailActivity : AppCompatActivity() {
+
 
     private val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
-        val imageView: ImageView = findViewById(R.id.movie_poster)
-        val imageviewdata = intent.extras?.getString("MovieId")
-        val textView : TextView = findViewById(R.id.movie_title)
-        Glide.with(imageView).load(IMAGE_BASE + imageviewdata).into(imageView)
-        textView.text = imageviewdata
+
+        val Movieposter : ImageView = findViewById(R.id.movie_poster)
+        val Moviebackdrop : ImageView = findViewById(R.id.movie_backdrop)
+        val Movietitle : TextView = findViewById(R.id.movie_title)
+        val Movierelease : TextView = findViewById(R.id.movie_release_date)
+        val Movieoverview : TextView = findViewById(R.id.movie_overview)
+        val Movierating : RatingBar = findViewById(R.id.movie_rating)
+
+        val Movieposterdata = intent?.extras?.getString(MOVIE_POSTER)
+        val Moviebackdropdata   = intent?.extras?.getString(MOVIE_BACKDROP)
+        val Movietitledata  = intent?.extras?.getString(MOVIE_TITLE)
+        val Moviereleasedata  = intent?.extras?.getString(MOVIE_RELEASE_DATE)
+        val Movieoverviewdata  = intent?.extras?.getString(MOVIE_OVERVIEW)
+        val Movieratingdata  = intent?.extras?.getString(MOVIE_RATING)
+
+        Movieoverview.text =Movieoverviewdata
+        Movietitle.text = Movietitledata
+        Movierelease.text = Moviereleasedata
+
+
+
+        Glide.with(Movieposter).load(IMAGE_BASE + Movieposterdata).into(Movieposter)
+        Glide.with(Moviebackdrop).load(IMAGE_BASE + Moviebackdropdata).into(Moviebackdrop)
+        Movierating.rating = Movieratingdata?.toFloat()!!/2
+
+
+
+
 
     }
 
--- app/src/main/java/com/example/cinemaflix/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/cinemaflix/adapters/MovieAdapter.kt
@@ -1,13 +1,13 @@
 package com.example.cinemaflix.adapters
 
 import android.content.Intent
+import android.os.Build.VERSION_CODES.S
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.cinemaflix.MovieDetailActivity
-import com.example.cinemaflix.R
+import com.example.cinemaflix.*
 import com.example.cinemaflix.models.Movie
 import kotlinx.android.synthetic.main.movie_item.view.*
 
@@ -16,11 +16,11 @@ private val movies : List<Movie>
 ) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>(){
 
     class MovieViewHolder(view : View) : RecyclerView.ViewHolder(view){
-        private val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
+        private val IMAGE_BASE = "https://image.tmdb.org/t/p/original/"
         fun bindMovie(movie : Movie){
             itemView.movie_title.text = movie.title
             itemView.movie_release_date.text = movie.release
-           itemView.rating.text = movie.vote_average
+            itemView.rating.text = (movie.vote_average?.toFloat()!!/2).toString()
             Glide.with(itemView).load(IMAGE_BASE + movie.poster).into(itemView.movie_poster)
         }
     }
@@ -38,7 +38,12 @@ private val movies : List<Movie>
         holder.itemView.setOnClickListener{
             val context = holder.itemView.context
             val intent =  Intent(context, MovieDetailActivity::class.java)
-            intent.putExtra("MovieId", movies[position].id)
+            intent.putExtra(MOVIE_BACKDROP, movies[position].backdropPath)
+            intent.putExtra(MOVIE_POSTER, movies[position].poster)
+            intent.putExtra(MOVIE_TITLE, movies[position].title)
+            intent.putExtra(MOVIE_RATING, movies[position].vote_average)
+            intent.putExtra(MOVIE_RELEASE_DATE, movies[position].release)
+            intent.putExtra(MOVIE_OVERVIEW, movies[position].overview)
             context.startActivity(intent)
         }
     }
